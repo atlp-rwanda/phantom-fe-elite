42358fe1c80d2a706a04e1297265ae192ef3da9f
tysepa <tuyiepa88@gmail.com>
2022-04-05 22:41:29 +0200

    Using jest to test
    Uses jest to test environment
    Set test on react app
    
    #27 testing functionality

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import InputForm from './InputForm'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome on React app</h1>
+      <InputForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe("display on landing page",()=>{
+
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('Welcome on React app');
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
