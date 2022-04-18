c673b8544e484575bb322fd323b963aa1b9ef661
rachelakimana <rachelakimana@gmail.com>
2022-04-18 20:17:12 +0200

    ft(Register): Register Drivers & operators
    Create register page and it's responsiveness
    display error message on unsuccessful attempts
    
    WIP #36

--- src/Routes/RegisterPage.js
+++ src/Routes/RegisterPage.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const RegisterPage = () => {
   return (
-    <div className="h-screen items-center bg-emerald-100 ">
-      <h1 className="text-center">REGISTER</h1>
-      <div className="flex flex-row h-2/3 w-full justify-center items-center mt-6 ">
-        <div className="h-full w-1/3 text-center bg-slate-500"></div>
-        <div className="h-full w-1/3 text-center bg-lime-500">
-          <form>
+    <div className="">
+      <h1 className="text-center text-2xl text-blue-500 font-semiboldb">
+        REGISTER
+      </h1>
+      <div className="flex flex-row  justify-center items-start mt-6 h-screen  ">
+        <div className="h-3/4 w-96 text-center bg-slate-500"></div>
+        <div className="flex justify-center items-start h-3/4 w-96 border border-blue-500 ">
+          <form className=" h-full w-80">
             <input
               type="text"
               value=""
               placeholder="Email"
-              className="mt-16 h-8 w-80"
+              className="mt-14 h-10 w-80 pl-3 border border-blue-500"
             />
             <br />
             <br />
@@ -20,16 +24,38 @@ const RegisterPage = () => {
               type="text"
               value=""
               placeholder="Username"
-              className="h-8 w-80"
+              className="h-10 w-80 pl-3 border border-blue-500"
             />
             <br />
             <br />
             <input
-              type="text"
-              value=""
+              type="password"
               placeholder="Password"
-              className="h-8 w-80"
+              className="h-10 w-80 pl-3 border border-blue-500 "
             />
+            <br />
+            <br />
+
+            <a href="" className=" ">
+              Reset Password
+            </a>
+            <br />
+            <br />
+            <button type="submit" className="h-10 w-80 bg-blue-500 text-white">
+              Register
+            </button>
+            <br />
+            <br />
+            <br />
+            <p className="flex flex-row">
+              or login with
+              <a href="" className="ml-4 ">
+                <FaLinkedin className="text-3xl text-blue-500" />
+              </a>
+              <a href="" className="ml-4">
+                <FcGoogle className="text-3xl" />
+              </a>
+            </p>
           </form>
         </div>
       </div>
