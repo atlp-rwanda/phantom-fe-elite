48418c082f051638f592271fa1615a12eb320f9f
nkunzi llc <nkunzillc@gmail.com>
2022-04-20 14:20:56 +0200

    Initial tailwindcss setup

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-		<nav class="container flex justify-around py-8 mx-auto bg-white">
+		<nav className="container flex justify-around py-8 mx-auto bg-white">
 			<div>
-				<h3 class="text-2xl font-medium text-blue-500">Phantom</h3>
+				<h3 className="text-2xl font-medium text-blue-500">Phantom</h3>
 			</div>
-			<div class="space-x-8">
+			<div className="space-x-8">
 				<Link to="/">Home</Link>
 				<Link to="/input">Input Form</Link>
 				<Link to="/about">About</Link>
