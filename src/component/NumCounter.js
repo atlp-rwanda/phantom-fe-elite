78c93c3ca7c9c28f87646f6532f257bf4fcdffb1
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-04-20 17:39:38 +0200

    chore(setup): setup tailwind
    - cleaning the code
    [ Finishes #3 ]

--- src/component/NumCounter.js
+++ src/component/NumCounter.js
@@ -1,7 +1,5 @@
-/** @format */
 
 import React from "react";
-// import "./counter.css";
 import { increaseValue, decreaseValue } from "../redux/actions/counterActions";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -12,7 +10,7 @@ const NumCounter = () => {
 	return (
 		<div className="holder josefinSans">
 			<button className="btn" onClick={() => dispatch(increaseValue())}>
-				Increase shhhhhhhjaaaddddddddd daskjashsfksah
+				Increase
 			</button>
 			<p>{numberState}</p>
 			<button className="btn" onClick={() => dispatch(decreaseValue())}>
