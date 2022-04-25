f86262a96d54c878f690a9b0c5862d5c35c38f8d
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-04-25 15:00:20 +0200

    feat(landing-page): resolving merge conflicts - second.
    - Resolving conflicts with merging 2.
    
    [ Finishes #28 ]

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './main.css'
 import { store } from "./redux/store/store";
 import { Provider } from "react-redux";
-=======
-
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./main.css";
-import { store } from "./redux/store/store";
-import { Provider } from "react-redux";
-
->>>>>>> ecd2377ae86d7b76b8330c637293d8a3a8aef964
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
