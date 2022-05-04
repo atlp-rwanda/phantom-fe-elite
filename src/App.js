4734d513d8d5e2a0ab6e2eaa20d418935b3d3354
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-04 09:52:21 +0200

    ch(Dashboard Layout): modified dashboard layout
    - made change to dashboard layout
    - made changes to routes navugation
    
    -- end --

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import RegisterPage from "./Routes/RegisterPage";
 import LandingPage from "./Routes/LandingPage";
-import Bus from "./views/admin/Bus";
-
+import Bus from "./views/operator/Bus";
 const App = () => {
 	return (
 		<BrowserRouter>
 			<div className="h-screen flex flex-col ">
-				<main className="mb-auto flex-grow">
-					<Routes>
-						<Route exact path="/" element={<LandingPage />}></Route>
-						<Route exact path="/register" element={<RegisterPage />}></Route>
-						<Route exact path="/bus" element={<Bus />}></Route>
-					</Routes>
-				</main>
+				<Routes>
+					<Route exact path="/" element={<LandingPage />}></Route>
+					<Route exact path="/register" element={<RegisterPage />}></Route>
+					<Route exact path="/bus" element={<Bus />}></Route>
+				</Routes>
 			</div>
 		</BrowserRouter>
 	);
--- src/Routes/RegisterPage.js
+++ src/Routes/RegisterPage.js
@@ -28,129 +28,133 @@ const RegisterPage = ({ onSubmit }) => {
 	const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 	return (
-    <>
-      <NavBar/>
-			<div className=" flex flex-col w-full h-full lg:w-full">
-				<h1 className="text-center text-2xl text-textBluePhant font-bold h-[8%]">
-					REGISTER
-				</h1>
+		<>
+			<NavBar />
+			<main className="mb-auto flex-grow">
+				<div className=" flex flex-col w-full h-full lg:w-full">
+					<h1 className="text-center text-2xl text-textBluePhant font-bold h-[8%]">
+						REGISTER
+					</h1>
 
-				<div
-					style={{ backgroundImage: `url(${secondimage})` }}
-					className="flex flex-row  justify-center items-start my-6 w-full h-[90%]">
 					<div
-						style={{ backgroundImage: `url(${trackroute})` }}
-						className="hidden md:flex text-center rounded-l-md  w-72 lg:w-80  h-full bg-no-repeat bg-cover"></div>
+						style={{ backgroundImage: `url(${secondimage})` }}
+						className="flex flex-row  justify-center items-start my-6 w-full h-[90%]">
+						<div
+							style={{ backgroundImage: `url(${trackroute})` }}
+							className="hidden md:flex text-center rounded-l-md  w-72 lg:w-80  h-full bg-no-repeat bg-cover"></div>
 
-					<div className="w-72 lg:w-80 h-full lg:h-full flex justify-center items-start  border border-darkBluePhant rounded-r-md  ">
-						<Formik
-							initialValues={{ email: "", username: "", password: "" }}
-							validationSchema={signUpSchema}
-							onSubmit={async (values) => {
-								setIsLoading(true);
-								onSubmit && onSubmit(values);
-								await sleep(2000);
-								setIsLoading(false);
-							}}>
-							{({
-								values,
-								errors,
-								touched,
-								handleChange,
-								handleBlur,
-								handleSubmit,
-							}) => (
-								<form className=" h-full w-5/6 pb-10 " onSubmit={handleSubmit}>
-									<input
-										type="email"
-										name="email"
-										data-testid="email-input"
-										placeholder="Email..."
-										onChange={handleChange}
-										onBlur={handleBlur}
-										value={values.email}
-										className="mt-5 lg:mt-6 md:mt-10 pl-3 h-10 lg:h-9 md:h-12 w-full  border border-darkBluePhant rounded text-md font-Montserrat"
-									/>
-									{touched.email && errors.email ? (
-										<span
-											data-testid="error-msg"
-											className="text-errorText text-xs">
-											{errors.email}
-										</span>
-									) : null}
+						<div className="w-72 lg:w-80 h-full lg:h-full flex justify-center items-start  border border-darkBluePhant rounded-r-md  ">
+							<Formik
+								initialValues={{ email: "", username: "", password: "" }}
+								validationSchema={signUpSchema}
+								onSubmit={async (values) => {
+									setIsLoading(true);
+									onSubmit && onSubmit(values);
+									await sleep(2000);
+									setIsLoading(false);
+								}}>
+								{({
+									values,
+									errors,
+									touched,
+									handleChange,
+									handleBlur,
+									handleSubmit,
+								}) => (
+									<form
+										className=" h-full w-5/6 pb-10 "
+										onSubmit={handleSubmit}>
+										<input
+											type="email"
+											name="email"
+											data-testid="email-input"
+											placeholder="Email..."
+											onChange={handleChange}
+											onBlur={handleBlur}
+											value={values.email}
+											className="mt-5 lg:mt-6 md:mt-10 pl-3 h-10 lg:h-9 md:h-12 w-full  border border-darkBluePhant rounded text-md font-Montserrat"
+										/>
+										{touched.email && errors.email ? (
+											<span
+												data-testid="error-msg"
+												className="text-errorText text-xs">
+												{errors.email}
+											</span>
+										) : null}
 
-									<br />
+										<br />
 
-									<input
-										type="text"
-										name="username"
-										placeholder="Username..."
-										value={values.username}
-										data-testid="username-input"
-										onChange={handleChange}
-										onBlur={handleBlur}
-										className="h-10 lg:h-9 md:h-12 w-full pl-3 mt-3 lg:mt-3 md:mt-6 border border-darkBluePhant rounded text-md font-Montserrat"
-									/>
-									{touched.username && errors.username ? (
-										<span
-											data-testid="error-msg"
-											className="text-errorText text-xs">
-											{errors.username}
-										</span>
-									) : null}
-									<br />
+										<input
+											type="text"
+											name="username"
+											placeholder="Username..."
+											value={values.username}
+											data-testid="username-input"
+											onChange={handleChange}
+											onBlur={handleBlur}
+											className="h-10 lg:h-9 md:h-12 w-full pl-3 mt-3 lg:mt-3 md:mt-6 border border-darkBluePhant rounded text-md font-Montserrat"
+										/>
+										{touched.username && errors.username ? (
+											<span
+												data-testid="error-msg"
+												className="text-errorText text-xs">
+												{errors.username}
+											</span>
+										) : null}
+										<br />
 
-									<input
-										type="password"
-										name="password"
-										placeholder="Password..."
-										data-testid="password-input"
-										value={values.password}
-										onBlur={handleBlur}
-										onChange={handleChange}
-										className="h-10 lg:h-9 md:h-12 w-full mt-3 lg:mt-3 md:mt-6 mb-4 pl-3 border border-darkBluePhant rounded text-md font-Montserrat"
-									/>
-									{touched.password && errors.password ? (
-										<span
-											data-testid="error-msg"
-											className="text-errorText text-xs">
-											{errors.password}
-										</span>
-									) : null}
-									<br />
+										<input
+											type="password"
+											name="password"
+											placeholder="Password..."
+											data-testid="password-input"
+											value={values.password}
+											onBlur={handleBlur}
+											onChange={handleChange}
+											className="h-10 lg:h-9 md:h-12 w-full mt-3 lg:mt-3 md:mt-6 mb-4 pl-3 border border-darkBluePhant rounded text-md font-Montserrat"
+										/>
+										{touched.password && errors.password ? (
+											<span
+												data-testid="error-msg"
+												className="text-errorText text-xs">
+												{errors.password}
+											</span>
+										) : null}
+										<br />
 
-									<a href="" className="font-Montserrat text-sm">
-										Reset Password
-									</a>
-									<br />
-
-									<button
-										type="submit"
-										data-testid="submit-form"
-										disabled={isLoading}
-										className={`h-10 lg:h-9 md:h-12 mt-2 lg:mt-2 md:mt-6 mb-3 w-full text-md ${
-											isLoading ? "bg-blue-100" : "bg-darkBluePhant"
-										} text-whitePhant rounded text-lg font-bold`}>
-										Register
-									</button>
-									<br />
-
-									<p className="flex flex-row mt-2 lg:mt-2 md:mt-10 font-Montserrat text-sm ">
-										or login with
-										<a href="" className="ml-4 ">
-											<FaLinkedin className="text-2xl text-darkBluePhant " />
+										<a href="" className="font-Montserrat text-sm">
+											Reset Password
 										</a>
-										<a href="" className="ml-4">
-											<FcGoogle className="text-2xl " />
-										</a>
-									</p>
-								</form>
-							)}
-						</Formik>
+										<br />
+
+										<button
+											type="submit"
+											data-testid="submit-form"
+											disabled={isLoading}
+											className={`h-10 lg:h-9 md:h-12 mt-2 lg:mt-2 md:mt-6 mb-3 w-full text-md ${
+												isLoading ? "bg-blue-100" : "bg-darkBluePhant"
+											} text-whitePhant rounded text-lg font-bold`}>
+											Register
+										</button>
+										<br />
+
+										<p className="flex flex-row mt-2 lg:mt-2 md:mt-10 font-Montserrat text-sm ">
+											or login with
+											<a href="" className="ml-4 ">
+												<FaLinkedin className="text-2xl text-darkBluePhant " />
+											</a>
+											<a href="" className="ml-4">
+												<FcGoogle className="text-2xl " />
+											</a>
+										</p>
+									</form>
+								)}
+							</Formik>
+						</div>
 					</div>
 				</div>
-      </div>
-      <FooterBlock/>
+			</main>
+			<FooterBlock />
 		</>
 	);
 };
