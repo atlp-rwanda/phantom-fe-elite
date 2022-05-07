d8de728c610f7c578e6d4691b49ae535ec57ef9f
Hodal Muheto <mhthodol@gmail.com>
2022-05-07 11:52:17 +0200

    feat(admin-panel):initial commit
    setup admin panel
    at the end of this feature
    admin  should be able to see a dashboard with statistical summary of buses, drivers, routes, and users
    
    featuer admin panel is WIP #8

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@
 /** @format */
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import Reset from "./Routes/Reset";
-import RegisterPage from "./Routes/RegisterPage";
-import LoginPage from "../src/component/login";
-import LandingPage from "./Routes/LandingPage";
-import ResetP from "./Routes/confirmation-password";
-
+import AdminPanel from "./component/Admin-panel/AdminPanel";
 const App = () => {
   return (
     <BrowserRouter>
       <div className="h-screen flex flex-col ">
-        {/* <NavBar /> */}
+       
         <main className="mb-12 flex-grow">
           <Routes>
-            <Route exact path="/" element={<LandingPage />}></Route>
-            <Route exact path="/login" element={<LoginPage />}></Route>
-            <Route exact path="/register" element={<RegisterPage />}></Route>
-            <Route path="/reset-password" element={<Reset />}></Route>
-            <Route path="/confirm-new-password" element={<ResetP />}></Route>
+            
+            <Route exact path="/" element={<AdminPanel />}></Route>
+          
           </Routes>
+
+
         </main>
-      </div>
-      {/* <FooterBlock /> */}
+        </div>
     </BrowserRouter>
   );
 };
