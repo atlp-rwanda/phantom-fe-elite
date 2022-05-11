b45860a92bbf193d6459532109d5862711158d9a
PrinceNiyonshuti <npprince47@gmail.com>
2022-05-11 11:21:49 +0200

    ch(Folder structure): made the app folder structure
    - added folder structure to follow
    - added routes layout
    
    -- end --

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 /** @format */
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import Reset from "./Routes/Reset";
-import RegisterPage from "./Routes/RegisterPage";
-import LoginPage from "../src/component/login";
-import LandingPage from "./Routes/LandingPage";
-import Bus from "./views/operator/Bus";
-import Layout from "./component/layout/Layout";
+import Router from "./Routes/Router";
 const App = () => {
 	return (
-		<BrowserRouter>
-			<div className="h-screen flex flex-col ">
-				<Routes>
-					<Route exact path="/" element={<LandingPage />}></Route>
-					<Route exact path="/register" element={<RegisterPage />}></Route>
-					<Route exact path="/bus" element={<Bus />}></Route>
-					<Route path="/reset-password" element={<Reset />}></Route>
-					<Route exact path="/login" element={<LoginPage />}></Route>
-					<Route exact path="/test" element={<Layout />}></Route>
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<Router />
 	);
 };
 
---
+++ src/Routes/Router.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Reset from "../views/Reset";
+import RegisterPage from "../views/RegisterPage";
+import LoginPage from "../views/login";
+import LandingPage from "../views/LandingPage";
+import Bus from "../views/operator/Bus";
+import ConfirmPassword from '../views/confirmation-password'
+const Router = () => {
+  return (
+		<BrowserRouter>
+			<div className="h-screen flex flex-col ">
+				<Routes>
+					<Route exact path="/" element={<LandingPage />}></Route>
+					<Route exact path="/register" element={<RegisterPage />}></Route>
+					<Route exact path="/bus" element={<Bus />}></Route>
+					<Route path="/reset-password" element={<Reset />}></Route>
+					<Route path="/confirm-new-password" element={<ConfirmPassword />}></Route>
+					<Route exact path="/login" element={<LoginPage />}></Route>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default Router
