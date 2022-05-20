4de39f4ede823d668f39c7eae71c886b245c865e
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-05-20 14:09:42 +0200

    refactor(CRUD): added better form

--- src/component/new_route/ListContent.js
+++ src/component/new_route/ListContent.js
@@ -3,113 +3,22 @@ import { date, number } from 'yup';
 import TableRow from './TableRow';
 
 
-const ListContent = (props) => {
+const ListContent = ({data, handleDelete}) => {
 
 let i = 0;
+
+const handling = (id) => {
+  handleDelete(id)
+}
+console.log();
 return (
  <>
-  <TableRow
-  origin = 'gasyata'
-  number = '1'
-  destination = 'Nyabugogo'
-  />
-  <TableRow
-  origin = 'gasyata'
-  number = '2'
-  destination = 'Nyabugogo'
-  />
-  <TableRow
-  origin = 'gasyata'
-  number = '3'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '4'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '5'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '6'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '7'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '8'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '9'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '10'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '11'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '12'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '13'
-  destination = 'Nyabugogo'
-  />
-    <TableRow
-  origin = 'gasyata'
-  number = '14'
-  destination = 'Nyabugogo'
-  />
-      <TableRow
-  origin = 'gasyata'
-  number = '15'
-  destination = 'Nyabugogo'
-  />
-      <TableRow
-  origin = 'gasyata'
-  number = '16'
-  destination = 'Nyabugogo'
-  />
-      <TableRow
-  origin = 'gasyata'
-  number = '17'
-  destination = 'Nyabugogo'
-  />
-      <TableRow
-  origin = 'gasyata'
-  number = '18'
-  destination = 'Nyabugogo'
-  />
-      <TableRow
-  origin = 'gasyata'
-  number = '19'
-  destination = 'Nyabugogo'
-  />
-
+  {data.map((dataItem) => {
+    i++
+    return <TableRow removeDeletedItem={handling} origin={dataItem.origin} number={i} id = {dataItem.id} handleDelete = {handling} destination={dataItem.destination} />;
+  })}
 </>
  );
 }
 
 export default ListContent
-
-
-
-
--- src/component/new_route/TableRow.js
+++ src/component/new_route/TableRow.js
@@ -1,33 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
 import UpdateRouteLine from '../UpdateRouteModal';
+import axios from 'axios';
 // modal toggle
+
 function toggleUpdate() {
-	document.getElementById("UpdateRouteLine").classList.toggle("hidden");
+  document.getElementById("UpdateRouteLine").classList.toggle("hidden");
 }
 
 
-function TableRow (props)  {
+function TableRow ({removeDeletedItem,...props})  {
+const [data, setData] = useState({});
+const [newRoute, setNewRoute] = useState({});
+const [dataFetched,setDataFetched] = useState(false);
 
+const fetchRoute = async () => {
+  setDataFetched(false);
+  const dataObject = await axios.get(`http://localhost:7000/routes/${props.id}`);
+  let oneRoute = dataObject.data;
+  setDataFetched(true);
+  return oneRoute;
+};
+
+
+
+
+
+const deleteHandle = async () => {
+  const dataObject = await axios.delete(`http://localhost:7000/routes/${props.id}`);
+  console.log(dataObject.data);
+  setData(dataObject.data)
+  removeDeletedItem(props.id);
+
+  // return dataObject;
+};
+
+
+
+const handleDeleting = () => {
+  props.handleDelete(data.id)
+}
+// const dataObject = fetchRoute()
 
   return (
-  
-  <>
-    <div className="grid grid-cols-12 border-b border-black border-solid">
-      <div className="col-span-1 pl-1">{props.number}</div>
+    <>
+      <div className="grid grid-cols-12 border-b border-black border-solid">
+        <div className="col-span-1 pl-1">{props.number}</div>
 
-      <div className="col-span-2 pl-1">{props.origin}</div>
-      <div className="col-span-1 pl-1">-</div>
-      <div className="col-span-6 pl-1">{props.destination}</div>
+        <div className="col-span-2 pl-1">{props.origin}</div>
+        <div className="col-span-1 pl-1">-</div>
+        <div className="col-span-6 pl-1">{props.destination}</div>
 
-      <div className="col-span-2 pl-3 flex justify-between w-1/2">
-        <FiEdit className=" w-4 h-4 cursor-pointer" data-testid ="updateRouteBtn" onClick={toggleUpdate} />
-        <RiDeleteBin6Line className=" text-red-500 w-5 h-5 cursor-pointer" onClick={() => {}}/>
+        <div className="col-span-2 pl-3 flex justify-between w-1/2">
+          <FiEdit
+            className=" w-4 h-4 cursor-pointer"
+            data-testid="updateRouteBtn"
+            onClick={async() => {
+              
+
+              fetchRoute().then(data=>{
+                console.log(data)
+                setNewRoute(data);
+              // toggleUpdate();
+                
+              });
+              
+            }}
+            
+          />
+          <RiDeleteBin6Line
+            className=" text-red-500 w-5 h-5 cursor-pointer"
+            onClick={deleteHandle}
+          />
+        </div>
       </div>
-    </div>
-    <UpdateRouteLine/>
-  </>
+      {dataFetched && (<UpdateRouteLine
+        update={newRoute}
+        setDataFetched={()=>setDataFetched(false)}
+      />)}
+    </>
   );
 }
 export default TableRow
