7bdbd98771531723c19f9a0c32166e411ac880cf
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-06-03 12:16:05 +0200

    feat(simulate-userview): Creating responsiveness version of the app and adding a marker
    - creating responsiveness version of the page
    - adding json server and configure it.
    - starting on the test
    - adding a marker using custom coordinates to show the current location of bus
    
    [ WIP #37 ]

--- src/user-view-stuff/TemplateComponent/HeaderUser.js
+++ src/user-view-stuff/TemplateComponent/HeaderUser.js
@@ -8,28 +8,28 @@ const HeaderUser = ({ setAsideOpen }) => {
   return (
     <header
       id="header-user-view"
-      className="bg-white col-start-1 sm:col-start-3 md:col-start-4 border-solid border-b-4 shadow border-adminFooterBackground lg:col-start-1 col-end-13  row-start-1 row-end-2 sm:row-end-3"
+      className="bg-white col-start-1 border-solid border-b-4 shadow border-adminFooterBackground lg:col-start-1 col-end-13  row-start-1 row-end-2 lg:row-end-3"
     >
       <section className="w-full flex justify-between items-center h-full px-3 sm:px-10">
-        <div className=" hidden sm:block w-[10%]">
+        <div className=" hidden lg:block w-[10%]">
           <img className="w-full" src={image} alt="" />
         </div>
         <div
-          className="sm:hidden text-black text-2xl"
+          className="lg:hidden text-black text-2xl"
           onClick={() => {
             setAsideOpen(true);
           }}
         >
           <RiMenuLine />
         </div>
-        <form action="" className="hidden sm:block w-2/5 h-7 mx-12">
+        <form action="" className="hidden sm:block md:w-2/5 lg:w-2/5 h-7 mx-12">
           <input
             type="text"
             placeholder="Search"
             className="w-full h-5/6 sm:h-full pl-2 border-1 border-solid border-[#6C757D] rounded-md"
           />
         </form>
-        <div className="flex justify-around w-3/4 sm:w-1/5 items-center h-full">
+        <div className="flex justify-around w-3/4 md:w-2/5 lg:w-1/4 xl:1/5 items-center h-full">
           <FaUserCircle className="text-[30px] sm:text-[40px] bg-white text-darkBluePhant my-1" />
           <div className="flex">
             <div className="flex flex-col">
