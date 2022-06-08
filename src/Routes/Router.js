a58963fd30b7e179522808df9162583e52bc613c
Benjamin shyaka <benshyaka99@gmail.com>
2022-06-08 09:16:07 +0200

    Ft(404-page and protecting routes): Protecting routes by redirecting to admins pages to login Redirect to 404 page when non found page reqested

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -8,24 +8,41 @@ import Bus from "../views/operator/Bus";
 import ConfirmPassword from '../views/confirmation-password'
 import UpdateOperator from "../views/operator/UpdateOperator";
 import UpdateDriver from "../views/driver/UpdateDriver";
+import PageNotFound from "../views/PageNotFound";
 
 const Router = () => {
-  	return (
-		<BrowserRouter>
-			<div className="h-screen flex flex-col ">
-				<Routes>
-					<Route exact path="/" element={<LandingPage />}></Route>
-					<Route exact path="/register" element={<RegisterPage />}></Route>
-					<Route exact path="/operator-bus" element={<Bus />}></Route>
-					<Route path="/reset-password" element={<Reset />}></Route>
-					<Route path="/confirm-new-password" element={<ConfirmPassword />}></Route>
-					<Route exact path="/login" element={<LoginPage />}></Route>
-					<Route exact path="/updateoperator" element={<UpdateOperator />}></Route>
-					<Route path="/updatedriver" element={<UpdateDriver />}></Route>
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+	var token = localStorage.getItem('token')
+	if (token) {
+		return (
+			<BrowserRouter>
+				<div className="h-screen flex flex-col ">
+					<Routes>
+						<Route exact path="/" element={<LandingPage />}></Route>
+						<Route exact path="/operator-bus" element={<Bus />}></Route>
+						<Route path="/reset-password" element={<Reset />}></Route>
+						<Route path="/confirm-new-password" element={<ConfirmPassword />}></Route>
+						<Route exact path="/updateoperator" element={<UpdateOperator />}></Route>
+						<Route path="/updatedriver" element={<UpdateDriver />}></Route>
+						<Route path="*" element={<PageNotFound />}></Route>
+					</Routes>
+				</div>
+			</BrowserRouter>
+		);
+	} else {
+		return (
+			<BrowserRouter>
+				<div className="h-screen flex flex-col ">
+					<Routes>
+						<Route exact path="/" element={<LandingPage />}></Route>
+						<Route exact path="/register" element={<RegisterPage />}></Route>
+						<Route exact path="/login" element={<LoginPage />}></Route>
+						<Route path="*" element={<PageNotFound />}></Route>
+					</Routes>
+				</div>
+			</BrowserRouter>
+		);
+	}
+  	
 }
 
 export default Router
