bee827351b74bda04f60c349fd663db7f32d4ce5
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-30 18:20:27 +0200

    ft(Sign in Integration): Sign in to Phantom Integration
    - The driver, administrator, and Operator should be able to sign in to app by providing their email & password.
    - After successfull signIn Attemps token should be stored in local storage
    - Appropriate error messages are displayed on unsuccessful attempts
    
      [ Finish #16]

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+// export const url = "https://chaoo-todo-app.herokuapp.com/api";
+export const url = "http://localhost:3001/api/v1";
+
+export const setHeaders = () => {
+	const headers = {
+		headers: {
+			"x-auth-token": localStorage.getItem("token"),
+		},
+	};
+
+	return headers;
+};
