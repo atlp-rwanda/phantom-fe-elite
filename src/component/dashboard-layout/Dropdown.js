8522dc92c3746327ce3b648b26f54c024c7cccf7
Hodal Muheto <mhthodol@gmail.com>
2022-07-11 23:55:17 +0200

    ft(logout):integration logout from account
    -Authenticated user should be able to logout.
    -Authenticated user should be able to access protected resourses/URLs
    
    feature logout integration #17

--- src/component/dashboard-layout/Dropdown.js
+++ src/component/dashboard-layout/Dropdown.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { IoMdLogOut } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 const Dropdown = () => {
-    let navigate =useNavigate()
+  let navigate = useNavigate();
+
+  const LogOut = () => {
+    alert("logout successfully");
+    localStorage.removeItem("token");
+    navigate("/login", { replace: true });
+  };
   return (
-    <div className='absolute top-20 right-10 bg-white border w-24 h-16 z-10'>
-        <ul>
-            <li>
-                <button className='text-center px-4 py-0' data-testid="button1">Settings</button>
-            </li>
-            <li>
-                <button className='text-center px-4 py-0' data-testid="button2" id='click-me' onClick={() => {
-                    localStorage.clear();
-                    navigate('/', {replace: true});
-                }}>Logout</button>
-            </li>
-        </ul>
+    <div className="absolute top-20 right-10 bg-white border w-24 h-16 z-10">
+      <ul>
+        <li>
+          <button className="text-center px-4 py-0" data-testid="button1">
+            Settings
+          </button>
+        </li>
+        <li>
+          <button
+            className="text-center px-4 py-0"
+            data-testid="button2"
+            id="click-me"
+            onClick={() => {
+              // localStorage.clear();
+              // navigate('/', {replace: true});
+              LogOut();
+            }}
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
     </div>
-  )
-}
-export default Dropdown
+  );
+};
+export default Dropdown;
