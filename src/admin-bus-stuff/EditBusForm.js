031399cf999c0ae0fa722cfe7fd58f1eb57b1a44
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-07-19 07:08:37 +0200

    feat(crud-operation-bus-integration):  Correcting typos in the newBusForm to clean the previous state.
        - Correcting the erasing of the form on cancellation of edit form after opening it
    
        [Finishes #23]

--- src/admin-bus-stuff/EditBusForm.js
+++ src/admin-bus-stuff/EditBusForm.js
@@ -131,9 +131,7 @@ const EditOperatorForm = ({
           <div className="bg-gray-200 px-4 py-2 mt-4 sm:mt-8 md:mt-5 lg:mt-9 rounded-b-md text-left flex">
             <button
               className="py-2 px-4 bg-gray-500 text-white rounded hover:bg-gray-600 mr-2"
-              onClick={() => {
-                setOpenModal(false);
-              }}
+              onClick={handleBack}
             >
               Back
             </button>
